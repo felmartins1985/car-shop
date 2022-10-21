e96ecc63b7990d1892271aa1cae4cde764ce70cc
Felipe <felmartins1985@gmail.com>
2022-10-21 16:40:24 -0300

    feat: requisito 19- criado endpoint motorcycle

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/error';
 import carRouter from './routes/car';
+import motorcycleRouter from './routes/motorcycle';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', carRouter);
+app.use('/motorcycles', motorcycleRouter);
 app.use(errorHandler);
 export default app;
