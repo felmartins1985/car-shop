0a0ee75127e416116e15b2b33866d0bcf0c71fbd
Felipe <felmartins1985@gmail.com>
2022-10-21 15:43:22 -0300

    feat: requisito 17- feito o endpoint /id para deletar

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -30,7 +30,7 @@ export default class CarController {
     res: Response<ICar>,
   ) {
     const result = await this._service.delete(req.params.id);
-    return res.status(200).json(result);
+    return res.status(204).json(result);
   }
   public async update(
     req: Request,
