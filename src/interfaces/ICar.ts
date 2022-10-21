7318facb3bbe3d4a5914d8ab001905254c9cf1f6
Felipe <felmartins1985@gmail.com>
2022-10-21 14:45:17 -0300

    feat: requisito 3- criado o ICar

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const CarZodSchema = VehicleZodSchema.extend({
+  doorsQty: z.number({
+    required_error: 'doorsQty is required',
+    invalid_type_error: 'doorsQty must be a number',
+  }).int({ message: 'Must be a integer' })
+    .positive({ message: 'Must be a positiver number' })
+    .lte(4, { message: 'doorsQty must be less than 4' })
+    .gte(2, { message: 'doorsQty must be greater than 2' }),
+  seatsQty: z.number({
+    required_error: 'seatsQty is required',
+    invalid_type_error: 'seatsQty must be a number',
+  }).int({ message: 'Must be a integer' })
+    .positive({ message: 'Must be a positiver number' })
+    .lte(7, { message: 'seatsQty must be less than 7' })
+    .gte(2, { message: 'seatsQty must be greater than 2' }),
+});
+type ICar = z.infer<typeof CarZodSchema>;
+export { CarZodSchema, ICar };
